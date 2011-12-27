5d4258bdf7790b65b8fe4713a9a8ee30907cab96
a.shigeru <a.shigeru@gmail.com>
2011-12-27 11:14:07 +0900

    GoogleAnalytics 外部化

---
+++ www/public/js/ga.js
@@ -0,0 +1,16 @@
+/**
+ * GoogleAnalytics タグの外部化
+ */
+( function(){
+	var _gaq = window._gaq || [];
+	_gaq.push(['_setAccount', 'UA-XXXXXXXX-1']);
+	_gaq.push(['_trackPageview']);
+	
+	window._gaq = _gaq;
+	
+	(function() {
+		var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+		ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+		var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+	})();
+})();
