c89bc26368973072265f4d05e9202da6f04c2c22
a.shigeru <a.shigeru@gmail.com>
2012-03-12 21:01:02 +0900

    TwitterHash フィルタ　リンク先変更

--- www/library/Budori/Filter/Twitter/Hash.php
+++ www/library/Budori/Filter/Twitter/Hash.php
@@ -1,10 +1,12 @@
 <?php
+require_once 'Zend/Filter/Interface.php';
+
 class Budori_Filter_Twitter_Hash implements Zend_Filter_Interface
 {
 	public function filter($string)
 	{
 		return 	preg_replace("/([\s]|^)\#([\w\-]+)/i"
-						, '$1<a href="http://twitter.com/search?q=%23$2" target="_blank">#$2</a>'
+						, '$1<a href="http://twitter.com/#!/search/realtime/%23$2" target="_blank">#$2</a>'
 						, $string);
 	}
 }
