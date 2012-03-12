7eef58380025b6c41b46b9286cf2d094f8df1528
a.shigeru <a.shigeru@gmail.com>
2012-03-12 21:01:19 +0900

    DenyWord バリデータ修正

--- www/library/Budori/Validate/DenyWord.php
+++ www/library/Budori/Validate/DenyWord.php
@@ -11,11 +11,15 @@ class Budori_Validate_DenyWord extends Zend_Validate_Abstract
 	 * 禁止ワードリスト
 	 * @var array
 	 */
-	protected $_denylist;
+	protected $_denylist = null;
 	
+	/**
+	 * 共通禁止ワードリスト
+	 * @var array
+	 */
+	protected static $_defaultDenyList = null;
 	
 	const DENY_EXISTS = 'denyExists';
-    
 	
     protected $_messageTemplates = array(
         self::DENY_EXISTS   => "deny word exists",
@@ -25,25 +29,57 @@ class Budori_Validate_DenyWord extends Zend_Validate_Abstract
     /**
      * 禁止ワードリストの初期化
      */
-    public function __construct($list)
+    public function __construct($list=null)
     {
 		if ($list instanceof Zend_Config) {
 			$list = $list->toArray();
 		}
 		
-		if (is_set($list['list'])) {
+		if ( isset( $list['list'] ) ) {
 			$list = $list['list'];
 		}
 		
-		$this->setPattern($list);
+		$this->setDenyList( $list );
 	}
 	
    	/**
    	 * 禁止ワードリストのセット
-   	 */
+	 * @param array $list
+	 */
 	public function setDenyList($list)
    	{
    		$this->_denylist = $list;
+   	}
+   	
+   	/**
+   	 * get denylist (or static list)
+   	 * @return array
+   	 */
+   	public function getDenyList()
+   	{
+   		if( !empty($this->_denylist) ){
+   			return $this->_denylist;
+   		}
+   		
+   		return self::$_defaultDenyList;
+   	}
+   	
+   	/**
+   	 * set static deny list
+   	 * @param array $list
+   	 */
+   	public static function setDefaultDenyList($list)
+   	{
+   		self::$_defaultDenyList = $list;
+   	}
+   	
+   	/**
+   	 * get static denylist
+   	 * @return array
+   	 */
+   	public static function getDefaultDenyList()
+   	{
+   		return self::$_defaultDenyList;
    	}
    	
     /**
@@ -56,12 +92,6 @@ class Budori_Validate_DenyWord extends Zend_Validate_Abstract
      */
     public function isValid($value)
     {
-    	
-    	if( empty( self::$_denylist ) ){
-    		require_once 'Zend/Validate/Exception.php';
-    		throw new Zend_Validate_Exception('not found denyWordList');
-		}
-		
 		$valueString = (string) $value;
 		
 		$this->_setValue($valueString);
@@ -70,10 +100,16 @@ class Budori_Validate_DenyWord extends Zend_Validate_Abstract
             return true;
         }
         
-        $record = self::$_denylist;
+        $words = $this->getDenyList();
         
-		foreach ( $record as $var ){
+    	if( empty( $words ) ){
+    		require_once 'Zend/Validate/Exception.php';
+    		throw new Zend_Validate_Exception('not found denyWordList');
+		}
+		        
+		foreach ( $words as $var ){
 			if( false !== stristr( $value,$var ) ){
+				
 	            $this->_error(self::DENY_EXISTS);
 				return false;
 			}
